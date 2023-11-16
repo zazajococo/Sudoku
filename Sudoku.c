4116ece96ccf8392ff0741d4086df7cdd850ae2a
Paul-Elouan <paulelouan.gl@gmail.com>
2023-11-16 12:19:24 +0100

    Plus de battrie lol

--- Sudoku.c
+++ Sudoku.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
+#define RED "\033[0;31m"
+#define RESET "\033[0m"
 #define TAILLE 9
 
 typedef int tGrille[TAILLE][TAILLE];
 
-int main() {
+void chargerGrille(tGrille g);
+void afficherGrille(tGrille grille);
+int possible(tGrille grille, int numLigne, int numColonne, int valeur);
+int grilleEstPleine(tGrille grille);
+
+int main()
+{
     tGrille grille1;
     int numLigne, numColonne, valeur;
 
@@ -32,7 +40,8 @@ int main() {
     return 0;
 }
 
-void chargerGrille(tGrille g){
+void chargerGrille(tGrille g)
+{
     char nomFichier[30];
     FILE * f;
 
@@ -49,39 +58,49 @@ void chargerGrille(tGrille g){
         fread(g, sizeof(int), TAILLE*TAILLE, f);
     }
     fclose(f);
+    /*
+    liste des grilles:
+    Grille1.sud
+    ...
+    Grille10.sud
+    */
 }
 
-void afficherGrille(tGrille grille) {
+void afficherGrille(tGrille grille)
+{
     printf("    1 2 3   4 5 6   7 8 9  \n");
     printf("  +-------+-------+-------+\n");
 
     for (int i = 0; i < TAILLE; ++i) {
         if (i % 3 == 0 && i != 0) {
-            printf("  +-------+-------+-------+\n");
+            printf("  +-------+-------+-------+\n"); // Affichage d'une ligne de separation pour les carres de 3x3
         }
 
-        printf("%d | ", i + 1);
+        printf("%d | ", i + 1); // Affichage des chiffres a droite (pour choisir la ligne)
 
-        for (int j = 0; j < TAILLE; ++j) {
-            if (j % 3 == 0 && j != 0) {
-                printf("| ");
+        for (int j = 0; j < TAILLE; ++j)
+        {
+            if (j % 3 == 0 && j != 0)
+            {
+                printf("| "); // Affichage d'une colonne de separation pour les carres de 3x3
             }
 
-            if (grille[i][j] == 0) {
-                printf(". ");
-            } else {
-                printf("%d ", grille[i][j]);
+            if (grille[i][j] == 0)
+            {
+                printf(". "); // Affichage d'un point si valeur vide
+            }
+            else
+            {
+                printf(RED "%d " RESET, grille[i][j]); // La valeur sinon
             }
         }
-
         printf("\n");
     }
-
     printf("  +-------+-------+-------+\n");
 }
 
-int possible(tGrille grille, int numLigne, int numColonne, int valeur) // Retournez 1 si possible, 0 sinon
+int possible(tGrille grille, int numLigne, int numColonne, int valeur) // Retourne 1 si possible, 0 sinon
 {}
 
-int grilleEstPleine(tGrille grille) // Retournez 1 si pleine, 0 sinon
+int grilleEstPleine(tGrille grille) // Retourne 1 si pleine, 0 sinon
 {}
